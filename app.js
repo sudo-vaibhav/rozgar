2dcf161de551e978cff6e8209bc94388cc031ae5
deepcr7 <deepeshsuranjan@gmail.com>
2020-08-01 13:32:30 +0530

    added user schema

--- app.js
+++ app.js
@@ -3,7 +3,15 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const app = express();
-app.use(bodyParser.json());
+app.use(bodyParser.json({ limit: "6mb" }));
+app.use(
+  bodyParser.urlencoded({
+    limit: "6mb",
+    extended: true,
+    parameterLimit: 50000,
+  })
+);;
+
 const worker = require("./routes/worker/worker");
 
 mongoose.set("useFindAndModify", false);
