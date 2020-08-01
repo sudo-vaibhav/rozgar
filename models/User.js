f030688b56803d8e2354087f235c8fd2328dd075
deepcr7 <deepeshsuranjan@gmail.com>
2020-08-01 13:31:34 +0530

    added user schema

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    isVerified: {
+      type: Boolean,
+      required: true,
+      default:false
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    mobile: {
+      type: String,
+      match: [/^\+91\d{10}$/, "Only Indian Numbers Allowed"],
+      required: true,
+      unique: true,
+    },
+    userAadhaarImage:{
+      type: String,
+      required:true
+    },
+    userUUID:{
+      type: String,
+      required:true,
+      unique:true
+    },
+    aadhaarNumber:{
+      type: String,
+      match:[/^\d{4}\s\d{4}\s\d{4}$/, "Only Valid Aadhaar Numbers are allowed"],
+      unique:true
+    },
+    organisations:{
+      type: [mongoose.Schema.Types.ObjectId],
+      ref: 'Organisation',
+      required:true
+    }
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
